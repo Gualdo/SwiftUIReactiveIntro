243d6e511af6c77e05e2a2ff01d520ec8f615de5
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2020-01-08 12:08:43 +0100

    Example done

--- SwiftUIReactiveIntro/ContentView.swift
+++ SwiftUIReactiveIntro/ContentView.swift
@@ -9,8 +9,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var users = [String]()
+    
+    var isInfoFilled: Bool {
+        return firstName.count > 0 && lastName.count > 0
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                VStack {
+                    VStack {
+                        Group {
+                            TextField("First Name", text: $firstName).padding(12)
+                            TextField("Last Name", text: $lastName).padding(12)
+                        }
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 5)
+                        Group {
+                            HStack {
+                                Button(action: {
+                                    self.users.append("\(self.firstName) \(self.lastName)")
+                                    self.firstName = ""
+                                    self.lastName = ""
+                                }) {
+                                    Group {
+                                        Text("Create User")
+                                            .foregroundColor(.white)
+                                            .padding(12)
+                                    }
+                                    .background(isInfoFilled ? Color.blue : Color.gray)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .shadow(radius: 5)
+                                }.disabled(isInfoFilled ? false : true)
+                                Button(action: {
+                                    self.users = []
+                                    self.firstName = ""
+                                    self.lastName = ""
+                                }) {
+                                    Group {
+                                        Text("Delete Users")
+                                            .foregroundColor(.white)
+                                            .padding(12)
+                                    }
+                                    .background(users.count > 0 ? Color.red : Color.gray)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .shadow(radius: 5)
+                                }.disabled(users.count > 0 ? false : true)
+                            }
+                        }
+                    }.padding(12)
+                }.background(Color.gray)
+                List(users, id: \.self) {
+                    Text($0)
+                }
+            }
+            .navigationBarTitle("Credit Card Form")
+            .navigationBarItems(leading:
+                VStack(alignment: .leading) {
+                    Text("Application State").font(.headline)
+                    HStack {
+                        Text("First Name:")
+                        Text(firstName).foregroundColor(.red)
+                        Text("Last Name:")
+                        Text(lastName).foregroundColor(.red)
+                    }
+                }
+            )
+        }
     }
 }
 
